ccec876a642f202931474510637506c081c00483
Konnor Welsch <konnor.welsch@eagles.ewu.edu>
2018-11-04 21:24:20 -0800

    Get scss and bootstrap working

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,37 @@
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
+    filename: "bundle.js",
+    path: __dirname + "/dist"
   },
 
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
   resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
 
   module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+      {
+        test: /\.scss$/,
+          use: [{
+            loader: "style-loader"
+          }, {
+            loader: "css-loader" 
+          }, {
+            loader: "sass-loader"
+          }]
+      }
+    ]
   },
 
   // When importing a module whose path matches one of the following, just
@@ -28,7 +39,7 @@ module.exports = {
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
+    "react": "React",
+    "react-dom": "ReactDOM"
   }
 };
